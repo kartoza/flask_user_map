02915de43ce15e4d3d53a12216f08731ec79c90b
Akbar Gumbira <akbargumbira@gmail.com>
2013-11-10 22:25:57 +0700

    JS file for supporting some function on user map.

---
+++ users/static/js/user-map-utilities.js
@@ -0,0 +1,15 @@
+/**
+ * Open an information modal. There is only one modal to use for showing information
+ * This function should be used if there is no other spesific behaviour about the modal.
+ * Element #information_modal is declared in base.html
+ * @param info_title: Title of the modal (usually set as 'Information')
+ * @param info_content: The content of information
+ */
+function showInformationModal(info_title, info_content) {
+  var information_modal = $('#information-modal');
+  information_modal.find(".modal-title").html(info_title);
+  information_modal.find("#info_content").html(info_content);
+  information_modal.modal({
+          backdrop: false
+  });
+}
